2989028401b4816e2c6a56c7688e69828f6b105f
AngelAlvDev <angelalv.alumn@cescristorey.com>
2020-10-09 19:02:58 +0200

    Ejercicio finalizado

---
+++ EjercicioBasico2/EjercicioBasico2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioBasico2
+{
+    class EjercicioBasico2
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Console.WriteLine("La suma de " + a + " y " + b + " es igual a " + (a - b));
+            Console.WriteLine("La resta de " + a + " y " + b + " es igual a " + (a - b));
+            Console.WriteLine("La multiplicación entre " + a + " y " + b + " es igual a " + (a * b));
+            Console.WriteLine("El módulo entre " + a + " y " + b + " es igual a " + (a % b));
+        }
+    }
+}
